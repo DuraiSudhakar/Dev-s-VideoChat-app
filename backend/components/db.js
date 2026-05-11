316078e494ff27d6882d0c545cf3af450d24aa9b
DuraiSudhakar <137634967+DuraiSudhakar@users.noreply.github.com>
2026-05-11 16:44:55 +0530

    Refactor database connection to use connection string

--- backend/components/db.js
+++ backend/components/db.js
@@ -7,11 +7,10 @@ dotenv.config(); // Load environment variables from .env
 const { Pool } = pg;
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+    connectionString: process.env.DB_URL,
+    ssl:{
+        rejectUnauthorized: false,
+    },
 });
 
 pool.on("error", (err) => {
